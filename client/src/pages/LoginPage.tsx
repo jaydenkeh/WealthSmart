ce5364c16b4daf3b1c29bd76e354aea19109e845
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-12 14:04:34 +0800

    wrapped LoginPage with FormContainer

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import FormContainer from "../components/FormContainer";
 
 const LoginPage = () => {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter your email" />
-      </Form.Group>
+    <FormContainer>
+      <h1>Login</h1>
+      <Form>
+        <Form.Group className="my-3" controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter your password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="my-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter your password" />
+        </Form.Group>
+        <Button className="my-3" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </FormContainer>
   );
 };
 
