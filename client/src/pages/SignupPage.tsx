28aa7e898c48eb1cc456a02a714dddf16974d266
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-12 16:21:15 +0800

    setup SignupPage

--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -1,32 +1,228 @@
-import React from "react";
+import { useRef, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import FormContainer from "../components/FormContainer";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = ""; // insert backend API url here
 
 const SignupPage = () => {
+  const errRef = useRef();
+
+  const [userName, setUsername] = useState("");
+  const [validName, setValidName] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+
+  const [password, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(userName));
+  }, [userName]);
+
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(email));
+  }, [email]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(password));
+    setValidMatch(password === matchPwd);
+  }, [password, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [userName, email, password, matchPwd]);
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
   return (
-    <FormContainer>
-      <h1>Sign Up</h1>
-      <Form>
-        <Form.Group className="my-3" controlId="userName">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="email" placeholder="Enter your username" />
-        </Form.Group>
+    <>
+      {success ? (
+        <section>
+          <h1>Success!</h1>
+          <p>
+            <a href="#">Sign In</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
+            {errMsg}
+          </p>
+          <FormContainer>
+            <h1>Sign Up</h1>
+            <Form>
+              <Form.Group className="my-3" controlId="userName">
+                <Form.Label>Username</Form.Label>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className={validName ? "valid" : "hide"}
+                />
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className={validName || !userName ? "hide" : "invalid"}
+                />
+                <Form.Control
+                  type="userName"
+                  placeholder="Enter your username"
+                  value={userName}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  aria-invalid={validName ? "false" : "true"}
+                  aria-describedby="uidnote"
+                />
+                <p
+                  id="uidnote"
+                  className={
+                    userName && !validName ? "instructions" : "offscreen"
+                  }
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  4 to 24 characters.
+                  <br />
+                  Must begin with a letter.
+                  <br />
+                  Letters, numbers, underscores, hyphens allowed.
+                </p>
+              </Form.Group>
 
-        <Form.Group className="my-3" controlId="email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter your email" />
-        </Form.Group>
+              <Form.Group className="my-3" controlId="email">
+                <Form.Label>Email address</Form.Label>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className={validEmail ? "valid" : "hide"}
+                />
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className={validEmail || !email ? "hide" : "invalid"}
+                />
+                <Form.Control
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  aria-invalid={validEmail ? "false" : "true"}
+                  aria-describedby="emailnote"
+                />
+                <p
+                  id="emailnote"
+                  className={
+                    email && !validEmail ? "instructions" : "offscreen"
+                  }
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  Must begin with a letter.
+                  <br />
+                  Letters, numbers, underscores allowed.
+                </p>
+              </Form.Group>
 
-        <Form.Group className="my-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter your password" />
-        </Form.Group>
-        <Button className="my-3" variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </FormContainer>
+              <Form.Group className="my-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className={validPwd ? "valid" : "hide"}
+                />
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className={validPwd || !password ? "hide" : "invalid"}
+                />
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPwd(e.target.value)}
+                  required
+                  aria-invalid={validPwd ? "false" : "true"}
+                  aria-describedby="passwordnote"
+                />
+                <p
+                  id="passwordnote"
+                  className={
+                    password && !validPwd ? "instructions" : "offscreen"
+                  }
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  8 to 24 characters.
+                  <br />
+                  Must include uppercase and lowercase letters, a number and a
+                  special character.
+                  <br />
+                  Allowed special characters:{" "}
+                  <span aria-label="exclamation mark">!</span>{" "}
+                  <span aria-label="at symbol">@</span>{" "}
+                  <span aria-label="hashtag">#</span>{" "}
+                  <span aria-label="dollar sign">$</span>{" "}
+                  <span aria-label="percent">%</span>
+                </p>
+              </Form.Group>
+              <Form.Group className="my-3" controlId="confirm-password">
+                <Form.Label>Confirm Your Password</Form.Label>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className={validMatch && matchPwd ? "valid" : "hide"}
+                />
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className={validMatch || !matchPwd ? "hide" : "invalid"}
+                />
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm your password"
+                  onChange={(e) => setMatchPwd(e.target.value)}
+                  value={matchPwd}
+                  required
+                  aria-invalid={validMatch ? "false" : "true"}
+                  aria-describedby="confirmnote"
+                />
+                <p
+                  id="confirmnote"
+                  className={!validMatch ? "instructions" : "offscreen"}
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  Must match the first password input field.
+                </p>
+              </Form.Group>
+              <Button
+                className="my-3"
+                variant="primary"
+                type="submit"
+                disabled={
+                  !validName || !validEmail || !validPwd || !validMatch
+                    ? true
+                    : false
+                }
+              >
+                Submit
+              </Button>
+            </Form>
+            <Form.Text muted>
+              Already registered? <Link to="/login">Sign In</Link>
+            </Form.Text>
+          </FormContainer>
+        </section>
+      )}
+    </>
   );
 };
 
