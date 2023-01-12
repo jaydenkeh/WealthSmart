8928743cd43cb3d89c8f090392ac7d1b1e78e01d
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-12 10:36:18 +0800

    setup NavigationBar.tsx template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  return (
+    <div className="App">
+      Hello!
+      <NavigationBar />
+    </div>
+  );
 }
 
 export default App;
