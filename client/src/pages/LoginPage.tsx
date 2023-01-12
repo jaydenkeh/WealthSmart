77f302500d2d5912587ead3d4dbe8ccfa2362ed3
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-12 13:56:35 +0800

    setup loginpage form with react-bootstrap

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="email">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter your email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Enter your password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default LoginPage;
