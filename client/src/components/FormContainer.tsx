4882c77dff0e7d9617223a385a5c58e60670130f
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-12 14:09:38 +0800

    setup SignupPage with react-bootstrap

--- client/src/components/FormContainer.tsx
+++ client/src/components/FormContainer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-
 interface Props {
   children: React.ReactNode;
 }
--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import FormContainer from "../components/FormContainer";
 
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      <Form>
+        <Form.Group className="my-3" controlId="userName">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="email" placeholder="Enter your username" />
+        </Form.Group>
+
+        <Form.Group className="my-3" controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" />
+        </Form.Group>
+
+        <Form.Group className="my-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter your password" />
+        </Form.Group>
+        <Button className="my-3" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default SignupPage;
