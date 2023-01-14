e37a3cb8052e1327be51c36bc4284f408ea71eaa
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-14 14:59:35 +0800

    setup Data interface for TopMarketCap component

--- client/src/components/TopMarketCap.tsx
+++ client/src/components/TopMarketCap.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+
+interface Data {
+  symbol: string;
+  companyName: string;
+  marketCap: number;
+  price: number;
+  volume: number;
+  exchangeShortName: string;
+  industry: string;
+}
 
 const FINANCIAL_MODELING_API_KEY = import.meta.env
   .VITE_FINANCIAL_MODELING_API_KEY;
 
 const url =
   "https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=75000000000&apikey=";
+
 const TopMarketCap: React.FC = () => {
   return <div>TopMarketCap</div>;
 };
