c3222876011f166c6e5006d75d7afb84138bd5ab
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-14 23:21:06 +0800

    setup SymbolPage route in App.tsx, and useParams in SymbolPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import HomePage from "./pages/HomePage";
+import SymbolPage from "./pages/SymbolPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="signup" element={<SignupPage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/symbol/:symbol" element={<SymbolPage />} />
         <Route
           path="*"
           element={<p>The page you are searching is not found</p>}
--- client/src/pages/SymbolPage.tsx
+++ client/src/pages/SymbolPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+const params = useParams();
+let test = params.symbol;
+console.log(test);
 
 const SymbolPage: React.FC = () => {
-  return <div>PaperTrading</div>;
+  return <div>Symbol Page</div>;
 };
 
 export default SymbolPage;
