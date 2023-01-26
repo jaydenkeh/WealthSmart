bd723b7a746e3c96569b41b1cec144f555d626f4
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-26 21:03:28 +0800

    edited MainDashboard and TopMarketCap CSS

--- client/src/components/MainDashboard.tsx
+++ client/src/components/MainDashboard.tsx
@@ -4,22 +4,18 @@ import TopGainersLosers from "./TopGainersLosers";
 
 //TODO set up toggles to toggle between the different markets
 //TODO set up i. Stock Price (Real time) for different exchanges(?)
-//TODO set up Most Active together with Top Gainers/Losers
-//! set up individual stock page to allow paper trading
-//! set up Portfolio page to display current traded tickers (to link to paper trading and show the current holdings?)
-//! set up Watchlist page to allow user to track stocks
 //TODO implement loading indicator to show up when data are still loading
 //TODO different tabs to set up - Stock Screener + News
 //? Implement ALPHA VANTAGE Search Query API for Search bar function
 
-// const ALPHA_VANTAGE_API_KEY = import.meta.env.VITE_ALPHA_VANTAGE_API_KEY;
-
 const MainDashboard: React.FC = () => {
   return (
     <>
-      <h2>US STOCK MARKET</h2>
-      <TopMarketCap />
-      <TopGainersLosers />
+      <div className="main-dashboard-container">
+        <h3>United States Stock Market</h3>
+        <TopMarketCap />
+        <TopGainersLosers />
+      </div>
     </>
   );
 };
--- client/src/components/TopMarketCap.tsx
+++ client/src/components/TopMarketCap.tsx
@@ -24,11 +24,9 @@ const MARKET_CAP_URL = `https://financialmodelingprep.com/api/v3/stock-screener`
 
 const TopMarketCap: React.FC = () => {
   const [market, setMarket] = useState<Data[] | null>(null);
-  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const fetchMarket = async () => {
-    setLoading(true); // to implement loading component
     try {
       const response = await axios.get<Data[]>(MARKET_CAP_URL, {
         params: {
@@ -45,21 +43,6 @@ const TopMarketCap: React.FC = () => {
       }
     } catch (err) {
       console.log(err);
-      // const response = await axios.get<Data[]>(MARKET_CAP_URL, {
-      //   params: {
-      //     marketCapMoreThan: 75000000000,
-      //     isEtf: false,
-      //     isActivelyTrading: true,
-      //     exchange: "NYSE,NASDAQ",
-      //     apikey: FINANCIAL_MODELING_API_KEY_2,
-      //   },
-      // });
-      // if (response) {
-      //   setMarket(response.data);
-      //   console.log(response.data);
-      // }
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -69,51 +52,53 @@ const TopMarketCap: React.FC = () => {
 
   return (
     <>
-      <h4>Largest companies by market capitalization</h4>
-      <Table striped>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>Name</th>
-            <th>Market cap (USD)</th>
-            <th>Price (USD)</th>
-            <th>Volume</th>
-            <th>Exchange</th>
-            <th>Industry</th>
-          </tr>
-        </thead>
-        <tbody>
-          {market &&
-            market
-              .filter((data) => data.volume && data.industry)
-              .map((data, i) => (
-                <tr key={i}>
-                  <td>
-                    <span
-                      className="symbol-button"
-                      onClick={() => navigate(`/symbol/${data.symbol}`)}
-                    >
-                      {data?.symbol}
-                    </span>
-                  </td>
-                  <td>{data?.companyName}</td>
-                  <td>
-                    {data?.marketCap
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                  </td>
-                  <td>{data?.price}</td>
-                  <td>
-                    {data?.volume
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                  </td>
-                  <td>{data?.exchangeShortName}</td>
-                  <td>{data?.industry}</td>
-                </tr>
-              ))}
-        </tbody>
-      </Table>
+      <div className="market-cap-container">
+        <h4>Largest companies by market capitalization</h4>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>Symbol</th>
+              <th>Name</th>
+              <th>Market cap (USD)</th>
+              <th>Price (USD)</th>
+              <th>Volume</th>
+              <th>Exchange</th>
+              <th>Industry</th>
+            </tr>
+          </thead>
+          <tbody>
+            {market &&
+              market
+                .filter((data) => data.volume && data.industry)
+                .map((data, i) => (
+                  <tr key={i}>
+                    <td>
+                      <span
+                        className="symbol-button"
+                        onClick={() => navigate(`/symbol/${data.symbol}`)}
+                      >
+                        {data?.symbol}
+                      </span>
+                    </td>
+                    <td>{data?.companyName}</td>
+                    <td>
+                      {data?.marketCap
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </td>
+                    <td>{data?.price}</td>
+                    <td>
+                      {data?.volume
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </td>
+                    <td>{data?.exchangeShortName}</td>
+                    <td>{data?.industry}</td>
+                  </tr>
+                ))}
+          </tbody>
+        </Table>
+      </div>
     </>
   );
 };
