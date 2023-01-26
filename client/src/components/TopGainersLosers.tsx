256aed3143181b261fcc0e0c4088429b14665607
Jayden Keh <zhaoxikeh@gmail.com>
2023-01-26 22:40:39 +0800

    updated MainDashboard, TopGainersLosers CSS

--- client/src/components/TopGainersLosers.tsx
+++ client/src/components/TopGainersLosers.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Table from "react-bootstrap/Table";
 import axios from "axios";
 
@@ -18,16 +19,23 @@ const FINANCIAL_MODELING_API_KEY_2 = import.meta.env
 const TopGainersLosers: React.FC = () => {
   const [gainers, setGainers] = useState<Data[] | null>(null);
   const [losers, setLosers] = useState<Data[] | null>(null);
-  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const today = new Date();
+  const options: Intl.DateTimeFormatOptions = {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  const formattedDate = today.toLocaleDateString("en-UK", options);
 
   const fetchGainLose = async () => {
     try {
       const [gainersResponse, losersResponse] = await axios.all([
         axios.get<Data[]>(
-          `https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=${FINANCIAL_MODELING_API_KEY}`
+          `https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=${FINANCIAL_MODELING_API_KEY_2}`
         ),
         axios.get<Data[]>(
-          `https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=${FINANCIAL_MODELING_API_KEY}`
+          `https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=${FINANCIAL_MODELING_API_KEY_2}`
         ),
       ]);
       if (gainersResponse && losersResponse) {
@@ -38,22 +46,6 @@ const TopGainersLosers: React.FC = () => {
       }
     } catch (err) {
       console.log(err);
-      // const [gainersResponse, losersResponse] = await axios.all([
-      //   axios.get<Data[]>(
-      //     `https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=${FINANCIAL_MODELING_API_KEY_2}`
-      //   ),
-      //   axios.get<Data[]>(
-      //     `https://financialmodelingprep.com/api/v3/stock_market/losers?apikey=${FINANCIAL_MODELING_API_KEY_2}`
-      //   ),
-      // ]);
-      // if (gainersResponse && losersResponse) {
-      //   setGainers(gainersResponse.data);
-      //   setLosers(losersResponse.data);
-      //   console.log(gainersResponse.data);
-      //   console.log(losersResponse.data);
-      // }
-    } finally {
-      setLoading(false);
     }
   };
 
@@ -63,54 +55,106 @@ const TopGainersLosers: React.FC = () => {
 
   return (
     <>
-      <h4>Today Top Gainers</h4>
-      <Table striped>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Change</th>
-            <th>Change %</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gainers &&
-            gainers.map((data, i) => (
-              <tr key={i}>
-                <td>{data?.symbol}</td>
-                <td>{data?.name}</td>
-                <td>{data?.price}</td>
-                <td>{data?.change}</td>
-                <td>{data?.changesPercentage}</td>
+      <div className="gainers-losers-container">
+        <div className="top-gainers-container">
+          <h4>{formattedDate} Top Gainers</h4>
+          <Table striped>
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Name</th>
+                <th>Price (USD)</th>
+                <th>Change (USD)</th>
+                <th>Change %</th>
               </tr>
-            ))}
-        </tbody>
-      </Table>
-      <h4>Today Top Losers</h4>
-      <Table striped>
-        <thead>
-          <tr>
-            <th>Symbol</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Change</th>
-            <th>Change %</th>
-          </tr>
-        </thead>
-        <tbody>
-          {losers &&
-            losers.map((data, i) => (
-              <tr key={i}>
-                <td>{data?.symbol}</td>
-                <td>{data?.name}</td>
-                <td>{data?.price}</td>
-                <td>{data?.change}</td>
-                <td>{data?.changesPercentage}</td>
+            </thead>
+            <tbody>
+              {gainers &&
+                gainers.map((data, i) => (
+                  <tr key={i}>
+                    <td>
+                      <span
+                        className="top-gainers-symbol-button"
+                        onClick={() => navigate(`/symbol/${data.symbol}`)}
+                      >
+                        {data?.symbol}
+                      </span>
+                    </td>
+                    <td>{data?.name}</td>
+                    <td>
+                      {data?.price
+                        .toFixed(2)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </td>
+                    <td>
+                      {data?.change
+                        .toFixed(2)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </td>
+                    <td>
+                      {data?.changesPercentage
+                        .toFixed(2)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      %
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </div>
+        <div className="top-losers-container">
+          <h4>{formattedDate} Top Losers</h4>
+          <Table striped>
+            <thead>
+              <tr>
+                <th>Symbol</th>
+                <th>Name</th>
+                <th>Price (USD)</th>
+                <th>Change (USD)</th>
+                <th>Change %</th>
               </tr>
-            ))}
-        </tbody>
-      </Table>
+            </thead>
+            <tbody>
+              {losers &&
+                losers.map((data, i) => (
+                  <tr key={i}>
+                    <td>
+                      <span
+                        className="top-losers-symbol-button"
+                        onClick={() => navigate(`/symbol/${data.symbol}`)}
+                      >
+                        {data?.symbol}
+                      </span>
+                    </td>
+                    <td>{data?.name}</td>
+                    <td>
+                      {data?.price
+                        .toFixed(2)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </td>
+                    <td>
+                      {data?.change
+                        .toFixed(2)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </td>
+                    <td>
+                      {data?.changesPercentage
+                        .toFixed(2)
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      %
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </div>
+      </div>
       ;
     </>
   );
