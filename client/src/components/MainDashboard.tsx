e26fce31f0a1c93ad755a037bd27fc6d34ccf759
Jayden Keh <zhaoxikeh@gmail.com>
2023-02-08 18:01:52 +0800

    removed comments from MainDashboard

--- client/src/components/MainDashboard.tsx
+++ client/src/components/MainDashboard.tsx
@@ -4,9 +4,7 @@ import TopGainersLosers from "./TopGainersLosers";
 
 //TODO set up toggles to toggle between the different markets
 //TODO set up i. Stock Price (Real time) for different exchanges(?)
-//TODO implement loading indicator to show up when data are still loading
 //TODO different tabs to set up - Stock Screener + News
-//? Implement ALPHA VANTAGE Search Query API for Search bar function
 
 const MainDashboard: React.FC = () => {
   return (
